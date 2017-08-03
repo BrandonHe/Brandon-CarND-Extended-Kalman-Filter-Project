08843c7ed24e6dbc8854c5658dcbb43e065374f3
BrandonHe <lovinghesl@hotmail.com>
2017-08-03 17:06:41 +0800

    fix OPENSSL_VERSION error

--- install-mac.sh
+++ install-mac.sh
@@ -7,8 +7,8 @@ patch CMakeLists.txt < ../cmakepatch.txt
 mkdir build
 export PKG_CONFIG_PATH=/usr/local/opt/openssl/lib/pkgconfig 
 cd build
-OPENSSL_VERSION="`pwd`openssl version -v | cut -d' ' -f2"
-cmake .. -DOPENSSL_ROOT_DIR=/usr/local/Cellar/$OPENSSL_VERSION/1.0.2l/ -DOPENSSL_LIBRARIES=/usr/local/Cellar/openssl/$OPENSSL_VERSION/lib/
+OPENSSL_VERSION=`openssl version -v | cut -d' ' -f2`
+cmake .. -DOPENSSL_ROOT_DIR=/usr/local/Cellar/openssl/$OPENSSL_VERSION/ -DOPENSSL_LIBRARIES=/usr/local/Cellar/openssl/$OPENSSL_VERSION/lib/
 make 
 sudo make install
 cd ..
